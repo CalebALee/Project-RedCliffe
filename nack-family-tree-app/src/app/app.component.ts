353154050236f126b115da0d45e1a8aa36512492
Caleb Lee <calee@cspire.com>
2023-07-02 15:58:58 -0500

    Adding D3.js to Angular

--- nack-family-tree-app/src/app/app.component.ts
+++ nack-family-tree-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3-selection';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'nack-family-tree-app';
+
 }
